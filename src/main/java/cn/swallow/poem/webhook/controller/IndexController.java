9d56ada9d4351f4e69b3c914e77ca3e1e0cc5192
swallowff <swallowff@163.com>
2020-06-08 17:05:18 +0800

    add test controller

---
+++ src/main/java/cn/swallow/poem/webhook/controller/IndexController.java
@@ -0,0 +1,21 @@
+package cn.swallow.poem.webhook.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author swallow
+ * @create 2020/6/8
+ * @description
+ */
+@RestController
+@RequestMapping(value = "/")
+public class IndexController {
+
+    @RequestMapping("/test")
+    public String testOK(){
+        return "test ok";
+    }
+
+
+}
